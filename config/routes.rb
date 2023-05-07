bdd364a33b5b8fe34e7611b833d22ea696311dd2
marloboyer <marlo.boyer7@yahoo.com>
2023-05-07 11:32:39 -0500

    added models with seeds for inital data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+  post "/snacks" => "snacks#create"
+  post "/locations" => "location#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: "Benny", email: "benny123@test.com", password: "password")
+
+User.create()
+
+Snack.create(name: "BLT Sandwich Chip", brand: "Lays", user_id: 1)
+
+Snack.create(name: "Summer Freeze", brand: "Mountain Dew", user_id: 1)
+
+Snack.create(name: "Blackout Cake Oreo", brand: "Oreo", user_id: 1)
+
+Location.create(name: "Walmart", snack_id: 1, address: "")
+
+Location.create(name: "Target", snack_id: 1, address: "")
+
+Location.create(name: "7-11 Gas Station", snack_id: 1, address: "")
+
+Review.create(snack_id: 1, user_id: 1, note: "Tasted like I was eating a real BLT sandwich", snack_rate: 4)
